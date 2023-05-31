4fa53ec59112c3f0986f350bee3c064d9825723d
Andre Hammons <dev@ahammons.me>
2023-05-31 02:41:47 -0700

    refactor: rename Paginate and InfoBlock components

--- components/About.vue
+++ components/About.vue
@@ -7,26 +7,26 @@
       <h1 class="text-center text-xl md:text-2xl xl:text-4xl text-near-black font-bold">
         Who Am I?
       </h1>
-      <PaginatedWrapper
+      <Paginate
         :num-of-pages="numOfPages"
         :active-page="page"
         @go-to-page="(pageNum) => (page = pageNum)"
         @prev-page="handlePrev"
         @next-page="handleNext"
       >
-        <HoverBlock
+        <InfoBlock
           v-show="page === 1"
           headline="Professionally"
           icon-name="mdi:certificate"
           :body-text="`I served as a submarine electronics technician in the United States Navy for 8 years, where I grew into a seasoned problem solver and an adept learner of new technologies and systems. I get inspired by the ingenuity and forward momentum found in the ever-growing tech industry, where I look forward to contributing and continuously developing new skills!`"
         />
-        <HoverBlock
+        <InfoBlock
           v-show="page === 2"
           headline="Personally"
           icon-name="mdi:heart"
           :body-text="`I am a proud father and husband who just cannot resist a great dad joke! I love board game nights and karaoke with good friends. I also enjoy spending time reading about and learning new tech online for inspiration.`"
         />
-      </PaginatedWrapper>
+      </Paginate>
     </div>
   </section>
 </template>
